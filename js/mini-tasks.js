f1d67afcb010798ec528a01864cd539918b34cc5
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2022-12-08 12:36:26 +0200

    Mini tasks 1

---
+++ js/mini-tasks.js
@@ -0,0 +1,25 @@
+/* Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console
+ */
+
+const kebaboKaina = 5;
+console.log(`Kebabo kaina yra ${kebaboKaina} EUR`);
+
+const traukinioBilietas = 7.5;
+console.log(`Kelionės kaina Kaunas-Vilnius - ${traukinioBilietas} EUR`);
+
+const zemesMase = 6*10**24;
+console.log(`Žemės masė apytikriai ~ ${zemesMase}kg`);
+
+/* Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console
+ */
+
+const kaimynoVardas = 'Jonas';
+console.log('Mano kaimynas yra ' + kaimynoVardas);
+
+const geriausiasPrekybosCentras = "Norfa";
+console.log("Praktiški žmonės renkasi už " + geriausiasPrekybosCentras);
+
+const gyvunas = "Katė";
+console.log(`Mano megstamiausias gyvūnas - ${gyvunas}`);
