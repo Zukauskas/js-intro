a134f3d61cf2ae7e69889440dbda78af30e666a9
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2022-12-08 11:08:14 +0200

    Numbers

--- js/02-numbers.js
+++ js/02-numbers.js
@@ -85,6 +85,8 @@ let pazymiuSuma = 0;
 
 console.log('Pažymių suma: ', pazymiuSuma);
 
+// pazymiuSuma = pazymiuSuma + p1
+
 pazymiuSuma += p1;
 console.log('Pažymių suma: ', pazymiuSuma);
 
@@ -96,3 +98,34 @@ console.log('Pažymių suma: ', pazymiuSuma);
 
 pazymiuSuma += p4;
 console.log('Pažymių suma: ', pazymiuSuma);
+
+const islaida1 = 10;
+const islaida2 = 20;
+const islaida3 = 25;
+const islaida4 = 35;
+
+let saskaita = 100;
+saskaita -= islaida1;
+console.log('Isleidau:', islaida1, '; saskaitoje liko:', saskaita);
+
+saskaita -= islaida2;
+console.log('Isleidau:', islaida2, '; saskaitoje liko:', saskaita);
+
+saskaita -= islaida3;
+console.log('Isleidau:', islaida3, '; saskaitoje liko:', saskaita);
+
+saskaita -= islaida4;
+console.log('Isleidau:', islaida4, '; saskaitoje liko:', saskaita);
+
+console.log('----------------------------------')
+
+let index = 0;
+
+index++;
+index++;
+index++;
+index++;
+index++;
+index++;
+
+console.log('index:', index);
