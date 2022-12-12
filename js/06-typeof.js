ccfe82a33ec0f5e327fd1602e7c10093bd158e61
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2022-12-12 10:52:34 +0200

    typeof

---
+++ js/06-typeof.js
@@ -0,0 +1,17 @@
+/* 
+    TYPEOF - nustato reikšmės tipą
+*/
+
+const n = 5; //number
+const s = 'hi'; // string
+const b = true; //boolean
+
+const nType = typeof n;
+const sType = typeof s;
+const bType = typeof b;
+
+
+console.log(n, nType);
+console.log(s, sType);
+console.log(b, bType);
+
