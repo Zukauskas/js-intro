c776384e421ec5dd30158b436d5451a3b3226c31
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2022-12-12 09:31:58 +0200

    Boolean

---
+++ js/04-boolean.js
@@ -0,0 +1,29 @@
+/* 
+    BOOLEAN (loginė reikšmė)
+    true (number -> 1)
+    false (number -> 0)
+*/
+
+const isSnowing = true;
+const isRainning = false;
+
+console.log('Ar sninga?', isSnowing);
+console.log('Ar lyja?', isRainning);
+
+// Įdomesni atvejai
+
+const luckyNumber = 13;
+const luckySentence = `My lucky number is ${luckyNumber}!`;
+console.log(luckySentence);
+
+const snowString = `Ar sninga? Ats: ${isSnowing}`;
+console.log(snowString);
+
+const rainString = `Ar lyja? Ats: ${isRainning}`;
+console.log(rainString);
+
+const penkiosTiesosSum = true + true + true + true + true;
+console.log('Tiesos:', penkiosTiesosSum);
+
+const penkiosMelaiSum = false + false + false + false + false;
+console.log('Tiesos:', penkiosMelaiSum);
