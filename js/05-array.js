5b7be3b1ba1ab419f909f6d0f8e6855203cd1bb1
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2022-12-12 10:17:18 +0200

    Arrays

---
+++ js/05-array.js
@@ -0,0 +1,53 @@
+/* 
+    ARRAY (list, sąrašas, matrica, matrix, masyvas)
+*/
+
+const emptyArray = [];
+console.log(emptyArray);
+
+const marks = [10, 2, 8, 4, 6];
+console.log(marks);
+
+const usersNames = ['Petras', 'Marytė', 'Jonas', 'Ona'];
+console.log(usersNames);
+
+const boolList = [true, false, true, true, false, false];
+console.log(boolList);
+
+
+// LABAI NEREKOMENDUOTINA MIKSTUOTI SKIRTINGO TIPO REIKMŠMES MASYVE
+const combo1 = ['Jonas', 99, true];
+console.log(combo1);
+
+
+const marksCount = marks.length;
+console.log(marksCount);
+
+const emptySize = emptyArray.length;
+console.log(emptySize);
+
+const money = [5, 10, 20, 5];
+console.log('Kupiūros:', money);
+
+let wallet = 0;
+
+wallet += money[0];
+wallet += money[1];
+wallet += money[2];
+wallet += money[3];
+
+console.log('Wallet:',wallet ,'pinigų');
+
+const marks2 = [10, 2, 8, 4, 6];
+
+let marksSum = 0;
+
+marksSum += marks2[0];
+marksSum += marks2[1];
+marksSum += marks2[2];
+marksSum += marks2[3];
+marksSum += marks2[4];
+
+console.log('Marks sum:', marksSum);
+
+console.log(NaN + null)
