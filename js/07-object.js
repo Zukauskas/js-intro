74000393a323b74cb51496dff68baa3287efeecf
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2022-12-12 11:51:26 +0200

    More about objects

--- js/07-object.js
+++ js/07-object.js
@@ -43,7 +43,7 @@ const student2 = {
 console.log(student2);
 
 
-//STANDARTINIS REIKSMES ITRAUKIMAS IS OBJEKTO
+//STANDARTINIS REIKSMES ITRAUKIMAS IS OBJEKTO -> obj['key']
 const stud1Name = student1['name'];
 const stud1Age = student1['age'];
 const stud1Marks = student1['marks'];
@@ -63,3 +63,60 @@ console.log(stud2Name);
 console.log(stud2Age);
 console.log(stud2Marks);
 console.log(stud2Status);
+
+//SUPAPRATSINTA OBJEKTO REIKSMES ISTRAUKIMO SINTAKSE -> obj.key
+
+const car = {
+    brand: 'Tesla',
+    model: 'S',
+    price: 100000,
+    currency: 'EUR',
+    color: 'red'
+}
+
+console.table(car);
+console.log(car['brand']);
+console.log(car.brand);
+
+const obj = {
+    simple: 'paprasta reikšmė',
+    'child-name': "Johny Boy"
+}
+
+console.log(obj.simple);
+console.log(obj['child-name']);
+
+const marks = student2.marks
+
+let sum = 0;
+sum += marks[1];
+sum += marks[2];
+sum += marks[3];
+sum += marks[4];
+
+const count = marks.length;
+
+const average = sum / count;
+console.log('Mark average:', average)
+
+const person = {
+    name: 'Petras',
+    age: 99,
+    children:[
+        {
+            name: 'Maryte',
+            age: 13
+        },
+        {
+            name: 'Jonas',
+            age: 27
+        },
+        {
+            name: 'Ona',
+            age: 41
+        }
+    ],
+}
+
+console.log(person.children[2].name);
+console.log(person.children.length);
