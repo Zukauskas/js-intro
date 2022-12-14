84fcd33b494158c9c62a860aebfb6f8bbf53ed85
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2022-12-14 15:32:29 +0200

    shop function solution

--- tasks/shop.js
+++ tasks/shop.js
@@ -1,33 +1,41 @@
-
-const valiuta = 'EUR';
+const valiuta = "EUR";
 const prekes = [
     {
-        name: 'Bananas',
-        price:  2,
-        inStock: 10
+        name: "Bananas",
+        price: 2,
+        inStock: 10,
     },
     {
-        name: 'Agurkas',
-        price:  3,
-        inStock: 10
+        name: "Agurkas",
+        price: 3,
+        inStock: 10,
     },
     {
-        name: 'Pomidoras',
-        price:  1.57,
-        inStock: 30
-    }
+        name: "Pomidoras",
+        price: 1.57,
+        inStock: 30,
+    },
 ];
 
-function shop (products,currency) {
-
+function shop(products, currency) {
+    let totalValue = 0;
+    console.log("MUSU PARDUOTUVE");
+    console.log("----------------");
+    for (let i = 0; i < products.length; i++) {
+        console.log(` 1) ${products[i].name} kainuoja ${products[i].price.toFixed(2)} ${currency} ir turime jų ${products[i].inStock} vienetų.`);
+        totalValue += products[i].price * products[i].inStock;
+    }
+    console.log("------------------------");
+    console.log(`Viso asortimento kaina: ${totalValue.toFixed(2)} ${currency}`);
 }
 
 shop(prekes, valiuta);
- 
-console.log('MUSU PARDUOTUVE');
+
+/* console.log('MUSU PARDUOTUVE');
 console.log('----------------');
 console.log(` 1) Bananas kainuoja 2.00 EUR ir turime jų 10 vienetų.`);
 console.log(` 2) Agurkas kainuoja 3.00 EUR ir turime jų 10 vienetų.`);
 console.log(` 3) Pomidoras kainuoja 1.57 EUR ir turime jų 30 vienetų.`);
 console.log('------------------------');
 console.log(`Viso asortimento kaina: 97.10 EUR`);
+ */
