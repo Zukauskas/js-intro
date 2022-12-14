f31fb5a0cec70d27489fcd264e9fdf382baecd8e
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2022-12-14 11:45:11 +0200

    shop task js

---
+++ tasks/shop.js
@@ -0,0 +1,33 @@
+
+const valiuta = 'EUR';
+const prekes = [
+    {
+        name: 'Bananas',
+        price:  2,
+        inStock: 10
+    },
+    {
+        name: 'Agurkas',
+        price:  3,
+        inStock: 10
+    },
+    {
+        name: 'Pomidoras',
+        price:  1.57,
+        inStock: 30
+    }
+];
+
+function shop (products,currency) {
+
+}
+
+shop(prekes, valiuta);
+ 
+console.log('MUSU PARDUOTUVE');
+console.log('----------------');
+console.log(` 1) Bananas kainuoja 2.00 EUR ir turime jų 10 vienetų.`);
+console.log(` 2) Agurkas kainuoja 3.00 EUR ir turime jų 10 vienetų.`);
+console.log(` 3) Pomidoras kainuoja 1.57 EUR ir turime jų 30 vienetų.`);
+console.log('------------------------');
+console.log(`Viso asortimento kaina: 97.10 EUR`);
