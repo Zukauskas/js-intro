07c77bfe7366b02ae6f388696dd12b701627ca54
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2022-12-14 10:19:57 +0200

    ifs

--- js/09-if.js
+++ js/09-if.js
@@ -9,6 +9,10 @@ console.clear();
     naudotini: >, <, <=, >=, ===, !==
     NEnaudotini: ==, !=
 
+    Loginiai operatoriai :
+    && - AND - tenkina visas salygas
+    || - OR - tenkina bent viena salyga
+
     Sablonai:
 
     if () {}
@@ -29,19 +33,19 @@ const b = 5;
 if (a == b) {
     console.log(`${a} lygu ${b}`);
 } else {
-    console.log(`${a} nera lygu ${b}`)
+    console.log(`${a} nera lygu ${b}`);
 }
 
 
 if (a <= b) {
     console.log(`${a} maziau arba lygu uz ${b}`);
 } else {
-    console.log(`${a} nera maziau arba lygu uz ${b}`)
+    console.log(`${a} nera maziau arba lygu uz ${b}`);
 }
 
 
 if (a != b) {
-    console.log(`${a} nera lygu ${b}`)
+    console.log(`${a} nera lygu ${b}`);
 } else {
     console.log(`${a} lygu ${b}`);
 }
@@ -65,18 +69,41 @@ if (c > d) {
 console.log('-----------Griežta------------');
 
 if ( 5 === 5) {
-    console.log('Taip')
+    console.log('Taip');
 }
 
 if (5 !== 3) {
-    console.log('Taip')
+    console.log('Taip');
 }
 
 console.log('--------------------');
 
 
 if (5 === '5') {
-    console.log('vienodi')
+    console.log('vienodi');
 } else {
-    console.log('skirtingi')
+    console.log('skirtingi');
 }
+
+console.clear();
+console.log('CLEAR 85');
+
+// code nesting
+
+const day = 4;
+
+if (day === 1) {
+    console.log('pirmadienis');
+} else {
+    if (day===2){
+        console.log('antradienis');
+    }else {
+        if (day===3){
+            console.log('treciadienis');
+        }else {
+            console.log('ketvirtadienis');
+        }
+    }
+}
+
+// if variable = true/false, then for "if" you can just write if(variable/!variable) instead of if(variable === true/false)
