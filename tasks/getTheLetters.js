ddf25e0f297686c0adb6d9445913207a953a00ca
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2022-12-16 00:36:41 +0200

    gettheLetters function with basic validation

---
+++ tasks/getTheLetters.js
@@ -0,0 +1,74 @@
+/* 
+
+priima du kintamuosius:
+pirmasis nurodo tekstą, su kuriuo reikės atlikti užduotį
+antrasis nurodo kas kelintą raidę išrinkti
+patikrinti, ar pirmasis kintamasis yra teksto tipo:
+jei ne, išvedame pranešimą “Pirmasis kintamasis yra netinkamo tipo.”
+priešingu atveju tęsiame darbą
+patikrinti, ar pirmasis kintamasis yra ne tuščias tekstas ir ne didesnis nei 100 simbolių:
+jei ne, išvedame pranešimą “Pirmojo kintamojo reikšmė yra netinkamo dydžio.”
+priešingu atveju tęsiame darbą
+patikrinti, ar antrasis kintamasis yra skaičiaus tipo:
+jei ne, išvedame pranešimą “Antrasis kintamasis yra netinkamo tipo.”
+priešingu atveju tęsiame darbą
+patikriname, ar antrojo kintamojo vertė yra didesnė už nulį:
+jei ne, išvedame pranešimą “Antrasis kintamasis turi būti didesnis už nulį.”
+priešingu atveju tęsiame darbą
+patikriname, ar antrojo kintamojo vertė yra ne didesnė už pirmojo kintamojo ilgį:
+jei ne, išvedame pranešimą “Antrasis kintamasis turi būti ne didesnis už pateikto teksto ilgį.”
+priešingu atveju tęsiame darbą
+išrenkame iš nurodyto teksto kas kelintą raidę (pagal antrojo kintamojo žingsnį)
+išrinktas raides sudėti į atskirą kintamąjį, kuris yra teksto tipo
+gražina rezultatą
+TESTAI:
+console.log( isrinktiRaides( “abcdefg”, 2 ) );
+rezultatas: “bdf”
+console.log( isrinktiRaides( “abcdefghijkl”, 3 ) );
+rezultatas: “cfil”
+console.log( isrinktiRaides( “abc”, 0 ) );
+rezultatas: “Antrasis kintamasis turi būti didesnis už nulį.”
+console.log( isrinktiRaides( “abc”, 4 ) );
+rezultatas: “Antrasis kintamasis turi būti ne didesnis už pateikto teksto ilgį.”
+console.log( isrinktiRaides( 1561, 2 ) );
+rezultatas: “Pirmasis kintamasis yra netinkamo tipo.”
+
+*/
+console.clear();
+function isrinktiRaides(string, whichLetters) {
+    if (typeof string !== "string") {
+        return console.log("Pirmasis kintamasis yra netinkamo tipo.");
+    }
+    if (string.length === 0 || string.length > 100) {
+        return console.log("Pirmas kintamasis yra netinkamo dydžio");
+    }
+    if (typeof whichLetters !== "number") {
+        return console.log("Antras kintamasis yra netinkamo tipo");
+    }
+    if (whichLetters <= 0) {
+        return console.log("Antrasis kintamasis turi būti didesnis už nulį.");
+    }
+    if (whichLetters > string.length) {
+        return console.log("Antrasis kintamasis turi būti ne didesnis už pateikto teksto ilgi");
+    }
+    let newString = "";
+    for (i = whichLetters - 1; i < string.length; i += whichLetters) {
+        newString += string[i];
+    }
+    console.log(newString);
+}
+
+isrinktiRaides("abcdefg", 2);
+// rezultatas: “bdf”
+
+isrinktiRaides("abcdefghijkl", 3);
+// rezultatas: “cfil”
+
+isrinktiRaides("abc", 0);
+// rezultatas: “Antrasis kintamasis turi būti didesnis už nulį.”
+
+isrinktiRaides("abc", 4);
+// rezultatas: “Antrasis kintamasis turi būti ne didesnis už pateikto teksto ilgį.”
+
+isrinktiRaides(1561, 2);
+// rezultatas: “Pirmasis kintamasis yra netinkamo tipo.”
