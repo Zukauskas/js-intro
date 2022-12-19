c006521860ef455edab903e1e215c3458248a9be
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2022-12-19 16:25:38 +0200

    validations to check

--- tasks/functions.js
+++ tasks/functions.js
@@ -3,44 +3,44 @@ console.clear();
 console.log("#####FUNKCIJOS#####");
 console.log("");
 
-/* Funkcija pavadinimu “tusciaFunkcija”:
-    nepriima jokių kintamųjų
-    neatlieka jokios vidinės logikos
-    gražina boolean tipo reikšmę “false”
-    TESTAS:
-        console.log( tusciaFunkcija() );
-        rezultatas: false
- */
+// /* Funkcija pavadinimu “tusciaFunkcija”:
+//     nepriima jokių kintamųjų
+//     neatlieka jokios vidinės logikos
+//     gražina boolean tipo reikšmę “false”
+//     TESTAS:
+//         console.log( tusciaFunkcija() );
+//         rezultatas: false
+//  */
 
-function tusciaFunkcija() {
-    return false;
-}
+// function tusciaFunkcija() {
+//     return false;
+// }
 
-console.log(tusciaFunkcija());
+// console.log(tusciaFunkcija());
 
-/* Funkcija pavadinimu “daugyba”:
-        priima du skaičiaus tipo kintamuosius
-    atskirame kintamajame įsimena sandaugos reikšmę
-    gražina saudaugos rezultatą
-    TESTAI:
-        console.log( daugyba( skaicius1, skaicius2 ) );
-        console.log( daugyba( skaicius3, skaicius2 ) );
-        console.log( daugyba( skaicius1, skaicius3 ) );
-        rezultatas: teisingos reikšmės;
- */
+// /* Funkcija pavadinimu “daugyba”:
+//         priima du skaičiaus tipo kintamuosius
+//     atskirame kintamajame įsimena sandaugos reikšmę
+//     gražina saudaugos rezultatą
+//     TESTAI:
+//         console.log( daugyba( skaicius1, skaicius2 ) );
+//         console.log( daugyba( skaicius3, skaicius2 ) );
+//         console.log( daugyba( skaicius1, skaicius3 ) );
+//         rezultatas: teisingos reikšmės;
+//  */
 
-function daugyba(a, b) {
-    const sandauga = a * b;
-    return sandauga;
-}
+// function daugyba(a, b) {
+//     const sandauga = a * b;
+//     return sandauga;
+// }
 
-const skaicius1 = 15;
-const skaicius2 = 24;
-const skaicius3 = -15;
+// const skaicius1 = 15;
+// const skaicius2 = 24;
+// const skaicius3 = -15;
 
-console.log(daugyba(skaicius1, skaicius2));
-console.log(daugyba(skaicius3, skaicius2));
-console.log(daugyba(skaicius1, skaicius3));
+// console.log(daugyba(skaicius1, skaicius2));
+// console.log(daugyba(skaicius3, skaicius2));
+// console.log(daugyba(skaicius1, skaicius3));
 
 /* Funkcija pavadinimu “skaitmenuKiekisSkaiciuje”:
         priima vieną kintamąjį
@@ -63,46 +63,51 @@ console.log(daugyba(skaicius1, skaicius3));
             rezultatas: “Pateikta netinkamo tipo reikšmė.”
  */
 
-function skaitmenuKiekisSkaiciuje(skaicius) {
-    if (typeof skaicius === "number" && Number.isNaN(skaicius) === false) {
-        const skaiciusIlgis = skaicius.toString().length;
-        if (Number.isInteger(skaicius)) {
-            return skaiciusIlgis;
-        } else {
-            return skaiciusIlgis - 1;
-        }
-    } else {
-        return "Pateikta netinkamo tipo reikšmė";
-    }
-}
+// function skaitmenuKiekisSkaiciuje(skaicius) {
+//     if (typeof skaicius === "number" && Number.isNaN(skaicius) === false) {
+//         const skaiciusIlgis = Math.abs(skaicius).toString().length;
+//         if (Number.isInteger(skaicius)) {
+//             return skaiciusIlgis;
+//         } else if (skaicius % 1 !== 0) {
+//             return skaiciusIlgis - 1;
+//         } else {
+//         }
+//     } else {
+//         return "Pateikta netinkamo tipo reikšmė";
+//     }
+// }
 
-console.log(skaitmenuKiekisSkaiciuje(5));
-// rezultatas: 1
-console.log(skaitmenuKiekisSkaiciuje(781));
-// rezultatas: 3
-console.log(skaitmenuKiekisSkaiciuje(37060123456));
-// rezultatas: 11
-console.log(skaitmenuKiekisSkaiciuje(337.4));
-//rezultatas: 4
-console.log(skaitmenuKiekisSkaiciuje(true));
-// rezultatas: “Pateikta netinkamo tipo reikšmė.”
-console.log(skaitmenuKiekisSkaiciuje("asd"));
-// rezultatas: “Pateikta netinkamo tipo reikšmė.”
-console.log(skaitmenuKiekisSkaiciuje(NaN));
-// rezultatas: “Pateikta netinkamo tipo reikšmė.”
+// console.log(skaitmenuKiekisSkaiciuje(5));
+// // rezultatas: 1
+// console.log(skaitmenuKiekisSkaiciuje(-781));
+// // rezultatas: 3
+// console.log(skaitmenuKiekisSkaiciuje(37060123456));
+// // rezultatas: 11
+// console.log(skaitmenuKiekisSkaiciuje(337.4));
+// //rezultatas: 4
+// console.log(skaitmenuKiekisSkaiciuje(-337.4));
+// //rezultatas: 4
+// console.log(skaitmenuKiekisSkaiciuje(100000000000000000000000)); //pasidaryti kad veiktu su šituo :D
+// //rezultatas: 4
+// console.log(skaitmenuKiekisSkaiciuje(true));
+// // rezultatas: “Pateikta netinkamo tipo reikšmė.”
+// console.log(skaitmenuKiekisSkaiciuje("asd"));
+// // rezultatas: “Pateikta netinkamo tipo reikšmė.”
+// console.log(skaitmenuKiekisSkaiciuje(NaN));
+// // rezultatas: “Pateikta netinkamo tipo reikšmė.”
 
 console.log("--------------------------------------");
 
-function didziausiasSkaiciusSarase(numbersArray) {
-    let biggest = numbersArray[0];
-    if (numbersArray.length === 0) {
-        console.log("Pateiktas sąrašas negali būti tuščias.");
-        return;
-    } else if (typeof numbersArray !== "object") {
-        console.log("Pateikta netinkamo tipo reikšmė.");
-        return;
+function biggestNumber(numbersArray) {
+    if (!Array.isArray(numbersArray)) {
+        return "KLAIDA: reikia masyvo";
     }
-    for (let i = 0; i < numbersArray.length; i++) {
+    if (numbersArray.length < 1) {
+        return "KLAIDA: masyvas negali buti tuscias";
+    }
+
+    let biggest = numbersArray[0];
+    for (let i = 1; i < numbersArray.length; i++) {
         if (biggest < numbersArray[i]) {
             biggest = numbersArray[i];
         }
@@ -110,17 +115,40 @@ function didziausiasSkaiciusSarase(numbersArray) {
     return biggest;
 }
 
-console.log(didziausiasSkaiciusSarase([1]));
-// rezultatas: 1
-console.log(didziausiasSkaiciusSarase([1, 2, 3]));
-// rezultatas: 3
-console.log(didziausiasSkaiciusSarase([-5, 78, 14, 0, 18]));
-// rezultatas: 78
-console.log(didziausiasSkaiciusSarase([69, 69, 69, 69, 66]));
-// rezultatas: 69
-console.log(didziausiasSkaiciusSarase([-1, -2, -3, -4, -5, -6, -7, -8]));
-// rezultatas: -1
-console.log(didziausiasSkaiciusSarase("pomidoras"));
-//rezultatas: “Pateikta netinkamo tipo reikšmė.”
-console.log(didziausiasSkaiciusSarase([]));
-//rezultatas: “Pateiktas sąrašas negali būti tuščias.”
+console.log(biggestNumber("pomidoras"));
+console.log(biggestNumber(true));
+console.log(biggestNumber());
+console.log(biggestNumber(5));
+console.log(biggestNumber(function () {}));
+console.log(biggestNumber(null));
+console.log(biggestNumber({}));
+console.log(biggestNumber([]));
+
+console.log(biggestNumber([0]), "-->", 0);
+console.log(biggestNumber([1, 2, 3]), "-->", 3);
+console.log(biggestNumber([-5, 78, 14, 0, 18]), "-->", 78);
+console.log(biggestNumber([69, 69, 69, 69, 66]), "-->", 69);
+console.log(biggestNumber([-1, -2, -3, -4, -5, -6, -7, -8]), "-->", -1);
+
+console.log(biggestNumber([1, 2, 3, Infinity]), "-->", 3);
+console.log(biggestNumber([1, 2, Infinity, 3]), "-->", 3);
+console.log(biggestNumber([1, Infinity, 2, 3]), "-->", 3);
+console.log(biggestNumber([Infinity, 1, 2, 3]), "-->", 3);
+
+console.log(biggestNumber([1, 2, 3, NaN]), "-->", 3);
+console.log(biggestNumber([1, 2, NaN, 3]), "-->", 3);
+console.log(biggestNumber([1, NaN, 2, 3]), "-->", 3);
+console.log(biggestNumber([NaN, 1, 2, 3]), "-->", 3);
+
+console.log(biggestNumber([1, 2, 3, true]), "-->", 3);
+console.log(biggestNumber([1, 2, true, 3]), "-->", 3);
+console.log(biggestNumber([1, true, 2, 3]), "-->", 3);
+console.log(biggestNumber([true, 0, -1, -2]), "-->", 0);
+
+console.log(biggestNumber([1, 2, 3, "true"]), "-->", 3);
+console.log(biggestNumber([1, 2, "true", 3]), "-->", 3);
+console.log(biggestNumber([1, "true", 2, 3]), "-->", 3);
+console.log(biggestNumber(["true", 0, -1, -2]), "-->", 0);
+
+console.log(biggestNumber(["true", true, NaN, [], [5], Infinity, -Infinity, 0]), "-->", 0);
+console.log(biggestNumber(["true", true, NaN, [], [5], Infinity, -Infinity]));
