348665302d7902212fb89ca4fbfd19829953420d
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2022-12-21 14:40:20 +0200

    for in

--- js/16-loops.js
+++ js/16-loops.js
@@ -57,3 +57,40 @@ function sumavimasForeach(mark) {
 
 marks.forEach(sumavimasForeach);
 console.log("ForEach 3:", sumForeach3 / marks.length);
+
+// FOR-IN iteruoja per objekta
+
+const user = {
+    name: "Petras",
+    age: 99,
+    isMarried: true,
+    favoriteNumber: 13,
+    children: 5,
+    grandChildren: 25,
+    key: "geltonas",
+};
+
+let sumForObj = 0;
+const userKeys = Object.keys(user);
+console.log(userKeys);
+
+for (let i = 0; i < userKeys.length; i++) {
+    const key = userKeys[i];
+    const value = user[key];
+    if (typeof value === "number" && isFinite(value) && value > 0) {
+        sumForObj += value;
+    }
+}
+
+console.log("For obj:", sumForObj, "->", 142);
+
+let sumForin = 0;
+
+for (const key in user) {
+    const value = user[key];
+    if (typeof value === "number" && isFinite(value) && value > 0) {
+        sumForin += value;
+    }
+}
+
+console.log("For-in:", sumForin, "->", 142);
