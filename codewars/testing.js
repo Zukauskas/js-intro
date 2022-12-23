9e2a54be5a4bc0d162493b9c34a92aee13232871
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2022-12-23 21:57:54 +0200

    js file for testing codewars code

---
+++ codewars/testing.js
@@ -0,0 +1,43 @@
+console.clear();
+/* 
+
+You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+*/
+
+/* function getMiddle(s) {
+    const half = s.length / 2;
+    if (Number.isInteger(half)) {
+        return s[half - 1] + s[half];
+    }
+
+    return s[Math.floor(half)];
+}
+
+console.log(getMiddle("tetst"), "-->", "es");
+ */
+/* 
+function maskify(cc) {
+    if (cc.length <= 4) {
+        return cc;
+    }
+    const firstpart = [...cc]
+        .slice(0, -4)
+        .map((a) => (a = "#"))
+        .join("");
+    const lastFour = cc.slice(-4);
+
+    return firstpart + lastFour;
+}
+
+console.log(maskify("4556364607935616"), "############5616");
+console.log(maskify("1"), "1");
+console.log(maskify("11111"), "#1111");
+ */
+
+function isTriangle(a, b, c) {
+    return !(a >= b + c || b >= a + c || c >= a + b);
+}
+
+console.log(isTriangle(11, 2, 2), true);
+console.log(isTriangle(7, 20, 2), false);
