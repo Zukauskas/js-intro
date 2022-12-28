33549fa784dcd23b60dedba36257bcf1f42001f3
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2022-12-28 21:59:36 +0200

    Shop work in progress

--- kioskas/Shop.js
+++ kioskas/Shop.js
@@ -1,3 +1,110 @@
-class Shop {}
+class Shop {
+  constructor(name, currency) {
+    this.name = name;
+    this.currency = currency;
+  }
+
+  intro() {
+    console.log(
+      `Hi we are "${this.name}".\nUse .items() method to get list of items to purchase.\nUse .order() method to get your order details.`
+    );
+  }
+
+  items() {
+    console.log(`Items for sale at "${this.name}":`);
+    console.log("====================");
+    for (const [index, product] of inventory.entries()) {
+      console.log(
+        `${index + 1}) ${
+          product.productName.slice(0, 1).toUpperCase() +
+          product.productName.slice(1).toLowerCase()
+        } - ${(product.productPrice / 100).toFixed(2)} ${this.currency};`
+      );
+    }
+    console.log("====================");
+  }
+
+  addItem(name, price) {
+    const newProduct = {
+      productName: name,
+      productPrice: price,
+      availability: true,
+    };
+    inventory.push(newProduct);
+
+    console.log(
+      `"${this.name}" sells ${name} for ${(price / 100).toFixed(2)} ${
+        this.currency
+      } now!`
+    );
+  }
+
+  updatePrice(name, price) {
+    for (const product of inventory) {
+      if (product.productName === name) {
+        product.productPrice = price;
+      }
+    }
+  }
+
+  createCart(name) {
+    const newCart = {
+      owner: name,
+      items: [],
+    };
+    carts.push(newCart);
+    console.log(`${name} have an open cart at "${this.name}"`);
+  }
+
+  addItemToCart(owner, productID, amount) {
+    let productOrder = { id: productID, count: amount };
+    for (const cart of carts) {
+      if (cart.owner === owner) {
+        cart.items.push(productOrder);
+      }
+    }
+  }
+
+  order(owner) {
+    for (const cart of carts) {
+      if (cart.owner === owner) {
+        console.log(cart);
+      }
+    }
+  }
+
+  orderPrice(owner) {
+    let cartValue = 0;
+    for (const cart of carts) {
+      if (cart.owner === owner) {
+        for (const item of cart.items) {
+          if (inventory[item["availability"]] === true) {
+            cartValue += inventory[item.id - 1].productPrice * item.count;
+          } else {
+            console.log(
+              `Item with ID ${inventory[item.id]} is no longer available`
+            );
+          }
+          cart["totalToPay"] = cartValue;
+        }
+      }
+
+      console.log(
+        `${owner} order: ${(cartValue / 100).toFixed(2)} ${this.currency}.`
+      );
+    }
+  }
+  removeItem(item) {
+    for (const product of inventory) {
+      if (product.productName === item) {
+        product["availability"] = false;
+      }
+    }
+  }
+
+  pay(owner, money) {}
+}
+let carts = [];
+let inventory = [];
 
 export { Shop };
--- kioskas/index.js
+++ kioskas/index.js
@@ -4,6 +4,7 @@ import { Shop } from "./Shop.js";
 
 const onlineKioskas = new Shop("Meskiuko kioskas", "EUR");
 
+console.clear();
 onlineKioskas.intro();
 // Hi, we are "Meskiuko kioskas".\nUse .items() method to get list of items to purchase.\nUse .order() method to get your order details.
 
@@ -35,7 +36,9 @@ onlineKioskas.items();
 // ====================
 
 onlineKioskas.updatePrice("bulves", 90);
+
 // "Meskiuko kioskas" updated price and sells bulves for 0.90 EUR now!
+onlineKioskas.items();
 
 onlineKioskas.createCart("Augustas");
 // Augustas have an open cart at "Meskiuko kioskas"!
@@ -75,63 +78,63 @@ onlineKioskas.order("Barbora");
 }
 */
 
-onlineKioskas.orderPrice("Augustas");
-// Augustas order: 4.40 EUR.
+// onlineKioskas.orderPrice("Augustas");
+// // Augustas order: 4.40 EUR.
 
-onlineKioskas.orderPrice("Barbora");
-// Barbora order: 5.50 EUR.
+// onlineKioskas.orderPrice("Barbora");
+// // Barbora order: 5.50 EUR.
 
-onlineKioskas.removeItem("obuolys");
+// onlineKioskas.removeItem("obuolys");
+// // No more obuolys at "Meskiuko kioskas"!
+
+// onlineKioskas.removeItem("morka");
 // No more obuolys at "Meskiuko kioskas"!
 
-onlineKioskas.removeItem("morka");
-// No more obuolys at "Meskiuko kioskas"!
+// onlineKioskas.pay("Augustas", 400);
+// // Need more money!
 
-onlineKioskas.pay("Augustas", 400);
-// Need more money!
+// onlineKioskas.pay("Augustas", 600);
+// // Here is your 1.60 EUR change!\nThank you for purchasing at "Meskiuko kioskas"!
 
-onlineKioskas.pay("Augustas", 600);
-// Here is your 1.60 EUR change!\nThank you for purchasing at "Meskiuko kioskas"!
-
-onlineKioskas.pay("Barbora", 550);
+// onlineKioskas.pay("Barbora", 550);
 // Thank you for purchasing at "Meskiuko kioskas"!
 
-onlineKioskas.addItemToCart("Augustas", 3, 1);
-// You can not add items to already paid cart!
+// onlineKioskas.addItemToCart("Augustas", 3, 1);
+// // You can not add items to already paid cart!
 
-onlineKioskas.createCart("John");
-// John have an open cart at "Meskiuko kioskas"!
+// onlineKioskas.createCart("John");
+// // John have an open cart at "Meskiuko kioskas"!
 
-onlineKioskas.addItemToCart("John", 3, 1);
-onlineKioskas.addItemToCart("John", 4, 2);
-onlineKioskas.addItemToCart("John", 5, 1);
+// onlineKioskas.addItemToCart("John", 3, 1);
+// onlineKioskas.addItemToCart("John", 4, 2);
+// onlineKioskas.addItemToCart("John", 5, 1);
 
-onlineKioskas.addItemToCart("John", 1, 1);
-// Item is out of stock!
+// onlineKioskas.addItemToCart("John", 1, 1);
+// // Item is out of stock!
 
-onlineKioskas.order("John");
-/*
-{
-    owner: 'John',
-    items: [
-        { id: 3, count: 1 },
-        { id: 4, count: 2 },
-        { id: 5, count: 1 },
-    ]
-}
-*/
+// onlineKioskas.order("John");
+// /*
+// {
+//     owner: 'John',
+//     items: [
+//         { id: 3, count: 1 },
+//         { id: 4, count: 2 },
+//         { id: 5, count: 1 },
+//     ]
+// }
+// */
 
-onlineKioskas.items();
+// onlineKioskas.items();
 
-onlineKioskas.orderPrice("John");
-// Augustas order: 4.70 EUR.
+// onlineKioskas.orderPrice("John");
+// // Augustas order: 4.70 EUR.
 
-onlineKioskas.shopSummary();
-// Summary for the "Meskiuko kioskas"
-// ====================
-// Items sold: 8
-// Orders completed: 2
-// Orders in progress: 1
-// Profit: 10.80 EUR
-// Possible profit: 5.60 EUR
-// ====================
+// onlineKioskas.shopSummary();
+// // Summary for the "Meskiuko kioskas"
+// // ====================
+// // Items sold: 8
+// // Orders completed: 2
+// // Orders in progress: 1
+// // Profit: 10.80 EUR
+// // Possible profit: 5.60 EUR
+// // ====================
