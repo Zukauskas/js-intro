a8f6e50b82c7a808cbb477353098ba22657a94d4
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-01-03 10:09:01 +0200

    Add wannaBePlayer method

--- basketball/Basketball.js
+++ basketball/Basketball.js
@@ -1,7 +1,13 @@
 class Basketball {
     constructor(league, teamSize) {
         this.league = league;
-        this.teamSize = teamSize
+        this.teamSize = teamSize;
+        this.players = [];
+    }
+
+    wannaBePlayer(player, costPerYear) {
+        const newPlayer = {name: player, cost: costPerYear}
+        this.players.push(newPlayer)
     }
 
 }
