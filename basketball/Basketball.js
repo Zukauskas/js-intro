e14e8d7511e08a2e1740b3354a5b8cddb79c3dad
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-01-05 12:35:47 +0200

    buyPlayer method update

--- basketball/Basketball.js
+++ basketball/Basketball.js
@@ -19,6 +19,12 @@ class Basketball {
     buyPlayer(teamID, playerID) {
         const team = this.teams[teamID - 1];
         const player = this.players[playerID - 1]
+        if (team.players.some(p => p === player)) {
+            return console.log(`"${team.name}" team can't add the same player twice`)
+        }
+        if (team.players.length === 3) {
+            return console.log(`"${team.name}" team can't add extra players to it's team.\nMaximum players per team is 3`)
+        }
         team.players.push(player)
         console.log(`"${team.name}" team just acquired new player ${player.name} for ${player.cost} cash/year!`)
     }
