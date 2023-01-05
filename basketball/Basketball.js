4a11cc6d263dde5348e0dab5b3f663196e6ad2c7
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-01-05 11:43:02 +0200

    additional methods

--- basketball/Basketball.js
+++ basketball/Basketball.js
@@ -15,5 +15,24 @@ class Basketball {
         const newTeam = { name: teamName, players: [] };
         this.teams.push(newTeam);
     }
+
+    buyPlayer(teamID, playerID) {
+        const team = this.teams[teamID - 1];
+        const player = this.players[playerID - 1]
+        team.players.push(player)
+        console.log(`"${team.name}" team just acquired new player ${player.name} for ${player.cost} cash/year!`)
+    }
+
+    teamValue(teamID) {
+        const team = this.teams[teamID - 1];
+        const teamPlayers = team.players;
+        let value = 0;
+        for (const player of teamPlayers) {
+            value += player.cost
+        }
+
+        console.log(`"${team.name}" team is paying ${value} cast/year for it's players.`)
+    }
+
 }
 export { Basketball };
